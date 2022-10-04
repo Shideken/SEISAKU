49ca06e79561cdab315e018c5c16f04dcf4b1c78
Shideken <106945368+Shideken@users.noreply.github.com>
2022-10-04 14:41:45 +0900

    SceneManager

---
+++ shooting/SceneManager.cpp
@@ -0,0 +1,15 @@
+#include"DxLib.h"
+#include"SceneManager.h"
+#include"AbstractScene.h"
+
+AbstractScene* SceneManager::Update() {
+	AbstractScene* p = mScene->Update();
+	if (p != mScene) {
+		delete mScene;
+		mScene = p;
+	}
+	return p;
+}
+void SceneManager::Draw() const {
+	mScene->Draw();
+}
