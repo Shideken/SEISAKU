ab376066c26c9bea4a82119afdcbc3786f0c1521
Shideken <106945368+Shideken@users.noreply.github.com>
2022-11-08 11:19:36 +0900

    Update Player.cpp

--- shooting/Player.cpp
+++ shooting/Player.cpp
@@ -6,13 +6,14 @@ Player::Player()
 {
 	x = 640;
 	y = 640;
+	speed = 20;
 	if (LoadDivGraph("Images/Akabei.png", 8, 8, 1, 32, 32, playerimage) == -1);
 }
 
 void Player::Move() 
 {
-	if (pad.g_NowKey & PAD_INPUT_RIGHT)x += 30;
-	if (pad.g_NowKey & PAD_INPUT_LEFT)x -= 30;
+	if (pad.g_NowKey & PAD_INPUT_RIGHT)x += speed;
+	if (pad.g_NowKey & PAD_INPUT_LEFT)x -= speed;
 
   
 }
